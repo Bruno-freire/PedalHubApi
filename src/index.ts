29c72e959c2a732a4d010708eeac945cf315a582
Bruno Freire <brunofreire1717@gmail.com>
2023-07-28 17:35:31 -0500

    prisma-client

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ dotenv.config()
 
 const prisma = new PrismaClient();
 const secret = process.env.JWT_TOKEN
-
+const port = process.env.PORT
 
 
 const app = express();
@@ -63,5 +63,5 @@ app.use(
   }),
 );
 
-await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await new Promise<void>((resolve) => httpServer.listen(port || { port: 4000 }, resolve));
 console.log(`🚀 Server ready at http://localhost:4000/graphql`);
