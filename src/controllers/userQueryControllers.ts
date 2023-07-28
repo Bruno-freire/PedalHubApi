d3378a074ac38ff8605d40fc8980d5bb9e5ab159
Bruno Freire <brunofreire1717@gmail.com>
2023-07-28 10:38:58 -0500

    feat: authentication for userSubscription

--- src/controllers/userQueryControllers.ts
+++ src/controllers/userQueryControllers.ts
@@ -20,7 +20,8 @@ export const users = async (parent: any, args: any, context: any): Promise<User[
   return usersWithSubscribedRides;
 };
   
-export const userSubscriptions = (parent: any, args: { user_id: number }): Promise<Subscription[]> => {
-  const { user_id } = args;
+export const userSubscriptions = (parent: any, args: any, context: any): Promise<Subscription[]> => {
+  if(!context.req.user) return null
+  const user_id = context.req.user.id
   return prisma.subscription.findMany({ where: { user_id: user_id }, include: {ride: true, user: true} });
 };
